7e0cfc5db669699fbe83fc6249bea564fd776977
uekitybalt <124322851+uekitybalt@users.noreply.github.com>
2024-05-03 09:00:07 +0000

    images header banner

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')  
+
 
 @app.route("/")
 def hello():
-    return "Hello Worm!"
+  return render_template("home.html")
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
-  
-
-
+  app.run(host="0.0.0.0", debug=True)
